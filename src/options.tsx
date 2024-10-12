41d0bb7738d7af9caceddd7d5f5588eaaab4dac2
pingfury <pingfury@outlook.com>
2024-10-12 14:12:54 +0800

    fix parse data

--- src/options.tsx
+++ src/options.tsx
@@ -20,6 +20,23 @@ async function search(keyword, lang) {
   return ""
 }
 
+async function searchSpecies(keyword) {
+  const url = `https://species.wikimedia.org/w/api.php?action=query&list=search&srsearch=${encodeURIComponent(keyword)}&format=json&srlimit=1`; // 只取第一个结果
+  const response = await fetch(url);
+  const data = await response.json();
+
+  if (data.query && data.query.search && data.query.search.length > 0) {
+    const pageTitle = data.query.search[0].title;
+    const pageUrl = `https://species.wikimedia.org/wiki/${encodeURIComponent(pageTitle)}`;
+
+    // 获取页面内容，这部分需要处理可能存在的跨域问题
+    const pageResponse = await fetch(pageUrl);
+    const pageText = await pageResponse.text();
+    return pageText
+  }
+  return ""
+}
+
 function parseSpeciesMd(tab) {
   let trs = {};
   console.log("data", tab);
@@ -34,6 +51,26 @@ function parseSpeciesMd(tab) {
   });
   console.log("trs md: ", trs);
   return trs
+}
+
+function parseMdSpecies(html) {
+  const parser = new DOMParser();
+  const doc = parser.parseFromString(html, 'text/html');
+  let doc_u = u(doc.body.children);
+  let title = doc_u.find("#firstHeading").text().trim();
+  let content = doc_u.find(".mw-content-ltr.mw-parser-output");
+
+  let item = {
+    title: title,
+  };
+
+  let content_p = content.find("p").first();
+  if (content_p) {
+    item["describe"] = content_p.textContent.trim();
+  } else {
+    item["describe"] = ""
+  }
+  return item
 }
 
 function parseMd(html) {
@@ -93,7 +130,7 @@ function parseMdfr(html) {
   let doc_u = u(doc.body.children);
   console.log(doc_u);
   let content = doc_u.find("#mw-content-text");
-  let tab = content.find(".infobox.biota");
+  let tab = content.find(".taxobox_classification");
   let md = parseSpeciesMd(tab);
   let title = doc_u.find(".mw-page-title-main").text().trim();
 
@@ -111,14 +148,32 @@ function parseMdfr(html) {
   return item
 }
 
+function parseSpeciesMdEs(tab) {
+  let trs = {};
+  let doc = u(tab);
+  console.log("data", tab);
+  doc.find("tbody > tr").each(function (el) {
+    console.log("tr children: ", el.children.length)
+    if (el.children.length == 2) {
+      console.log("es md: ", el.textContent);
+      let [k, v] = el.textContent.split("\n").map(function (s) {
+        return s.trim().replace(":", "").replace("：", "")
+      })
+      trs[k] = v;
+    }
+  });
+  console.log("trs md: ", trs);
+  return trs
+}
+
 function parseMdes(html) {
   const parser = new DOMParser();
   const doc = parser.parseFromString(html, 'text/html');
   let doc_u = u(doc.body.children);
   console.log(doc_u);
   let content = doc_u.find("#mw-content-text");
-  let tab = content.find(".infobox.biota");
-  let md = parseSpeciesMd(tab);
+  let tab = content.find(".infobox").nodes[0];
+  let md = parseSpeciesMdEs(tab);
   let title = doc_u.find(".mw-page-title-main").text().trim();
 
   let item = {
@@ -136,28 +191,31 @@ function parseMdes(html) {
 }
 
 function OptionsIndex() {
-  const [data, setData] = useState("")
+  const [data, setData] = useState([])
   const [result, setResult] = useState([])
 
   const se = function (kw: String) {
     (async () => {
+      let html_text_species = await searchSpecies(kw);
       let html_text_zh = await search(kw, "zh");
       let html_text_en = await search(kw, "en");
       let html_text_fr = await search(kw, "fr");
       let html_text_es = await search(kw, "es");
+
+      let species_md = parseMdSpecies(html_text_species);
       let zh_md = parseMd(html_text_zh);
       let en_md = parseMdEn(html_text_en);
       let fr_md = parseMdfr(html_text_fr);
-      let es_md = parseMdfr(html_text_es);
+      let es_md = parseMdes(html_text_es);
       let item = {
-        "name": kw,
+        "name": species_md.title,
         "zh": zh_md,
         "en": en_md,
         "fr": fr_md,
         "es": es_md
       };
       console.log("item: ", item);
-      setResult([item]);
+      setResult([...result, item]);
     })();
   }
 
@@ -167,20 +225,41 @@ function OptionsIndex() {
         {result.map((item, index) => (
           <tr key={index}>
             <th>{index + 1}</th>  {/* 序号 */}
-            <td>{item.name}</td> {/* 学名 */}
-            <td>{item.en.title}</td> {/* 名称(en)*/}
-            <td>{item.en.metadata["Family"]}</td> {/* 科名(en) */}
-            <td>{item.en.metadata["Genus"]}</td> {/* 属名(en) */}
-            <td>{item.en.describe}</td> {/* 描述(en)*/}
-            <td>{item.zh.title}</td>  {/* 名称(zh)*/}
-            <td>{item.zh.metadata["科"]}</td> {/* 科名(zh) */}
-            <td>{item.zh.metadata["属"]}</td> {/* 属名(zh) */}
-            <td>{item.zh.describe}</td> {/* 描述(zh)*/}
+            <td><span>{item.name}</span></td> {/* 学名 */}
+
+            <td><span>{item.en.title}</span></td> {/* 名称(en)*/}
+            <td><span>{item.en.metadata["Family"]}</span></td> {/* 科名(en) */}
+            <td><span>{item.en.metadata["Genus"]}</span></td> {/* 属名(en) */}
+            <td><p>{item.en.describe}</p></td> {/* 描述(en)*/}
+
+            <td><span>{item.zh.title}</span></td>  {/* 名称(zh)*/}
+            <td><span>{item.zh.metadata["科"]}</span></td> {/* 科名(zh) */}
+            <td><span>{item.zh.metadata["属"]}</span></td> {/* 属名(zh) */}
+            <td><p>{item.zh.describe}</p></td> {/* 描述(zh)*/}
+
+            <td><span>{item.fr.title}</span></td>  {/* 名称(fr)*/}
+            <td><span>{item.fr.metadata["Ordre"]}</span></td> {/* 科名(fr) */}
+            <td><span>{item.fr.metadata["Famille"]}</span></td> {/* 属名(fr) */}
+            <td><span>{item.fr.describe}</span></td> {/* 描述(fr)*/}
+
+            <td><span>{item.es.title}</span></td>  {/* 名称(es)*/}
+            <td><span>{item.es.metadata["Familia"]}</span></td> {/* 科名(es) */}
+            <td><span>{item.es.metadata["Género"]}</span></td> {/* 属名(es) */}
+            <td><p>{item.es.describe}</p></td> {/* 描述(es)*/}
+
           </tr>
         ))}
       </tbody>
     );
   };
+
+  const handleKeyDown = (event) => {
+    if (event.key === 'Enter') {
+      console.log('Enter key pressed:', "回车");
+      data.forEach(kw => se(kw));
+    }
+  };
+
 
   return (
     <div className="w-screen h-screen">
@@ -189,13 +268,14 @@ function OptionsIndex() {
       </div>
       <div className="container mx-auto p-8">
         <label className="input input-bordered flex items-center gap-2">
-          <input type="text" className="grow" placeholder="Search" onChange={(e) => setData(e.target.value)} />
+          <input type="text" className="grow" placeholder="Search" onChange={(e) => setData(e.target.value.split("\n"))}
+            onKeyDown={handleKeyDown} />
           <svg
             xmlns="http://www.w3.org/2000/svg"
             viewBox="0 0 16 16"
             fill="currentColor"
             className="h-4 w-4 opacity-70"
-            onClick={(e) => se(data)}
+            onClick={(_) => data.forEach(kw => se(kw))}
           >
             <path
               fillRule="evenodd"
@@ -220,6 +300,16 @@ function OptionsIndex() {
                   <th>科名(zh)</th>
                   <th>属名(zh)</th>
                   <th>描述(zh)</th>
+
+                  <th>名称(fr)</th>
+                  <th>科名(fr)</th>
+                  <th>属名(fr)</th>
+                  <th>描述(fr)</th>
+
+                  <th>名称(es)</th>
+                  <th>科名(es)</th>
+                  <th>属名(es)</th>
+                  <th>描述(es)</th>
                 </tr>
               </thead>
               <ItemList />
@@ -227,7 +317,7 @@ function OptionsIndex() {
           </div>
         </div>
       </div>
-    </div>
+    </div >
   )
 }
 
